713118b0623d6370c7a75f0ec3fb2f6168f54e81
Divyaansh Mehta <63413879+aristodemus101@users.noreply.github.com>
2025-10-20 01:12:20 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Career Mentorship Program 8.0',
+  description: 'Student Performance Dashboard',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
